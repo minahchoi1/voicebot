793acdff11d85947145027331612606d8f984225
minahchoi1 <134687314+minahchoi1@users.noreply.github.com>
2024-06-04 15:57:36 +0900

    Add files via upload

--- voicebot.py
+++ voicebot.py
@@ -103,7 +103,7 @@ def main():
 
         # Open AI API 키 입력받기
         openai.api_key = st.text_input(label="OPENAI API 키", placeholder="Enter Your API Key", 
-                                       value="sk-proj-dyD2uoM8gkIS2SkYSveHT3BlbkFJ9mLnadFqb0k5Wgu8gNLT", type="password")
+                                       value="", type="password")
         st.markdown("---")
 
         # GPT 모델을 선택하기 위한 라디오 버튼 생성
